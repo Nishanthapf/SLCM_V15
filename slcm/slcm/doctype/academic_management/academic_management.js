298f1d99201c665dff63e35702708b300d0665b4
Nishanth Clinton A <nishanth.a@azimpremjifoundation.org>
2026-01-27 05:00:53 -0500

    update

--- slcm/slcm/doctype/academic_management/academic_management.js
+++ slcm/slcm/doctype/academic_management/academic_management.js
@@ -1,369 +1,215 @@
 // Copyright (c) 2026, CU and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Academic Management', {
-    refresh: function (frm) {
-        frm.trigger('render_terms_ui');
-        frm.trigger('render_class_ui');
-    },
+frappe.ui.form.on("Academic Management", {
+	refresh(frm) {
+		frm.trigger("render_terms_ui");
+		frm.trigger("render_class_ui");
+	},
 
-    render_terms_ui: function (frm) {
-        const $wrapper = frm.get_field('terms_ui_container').$wrapper;
-        $wrapper.html('<p>Loading Terms...</p>');
+	/* --------------------------------------------------------------------------
+	 * TERMS UI
+	 * -------------------------------------------------------------------------- */
+	render_terms_ui(frm) {
+		const $wrapper = frm.get_field("terms_ui_container").$wrapper;
+		$wrapper.html("<p>Loading Terms...</p>");
 
-        frappe.call({
-            method: 'frappe.client.get_list',
-            args: {
-                doctype: 'Term Configuration',
-                fields: ['name', 'term_name', 'academic_year', 'starts', 'ends', 'system', 'sequence'],
-                limit_page_length: 100,
-                order_by: 'starts desc'
-            },
-            callback: function (r) {
-                const terms = r.message || [];
-                let html = `
-                    <div class="row" style="margin-bottom: 15px;">
-                        <div class="col-xs-12 text-right">
-                            <button class="btn btn-primary btn-add-term">
-                                ${frappe.utils.icon('add', 'sm')} Add Term
-                            </button>
-                        </div>
-                    </div>
-                    <table class="table table-bordered">
-                        <thead style="background-color: #f5f7fa;">
-                            <tr>
-                                <th>Term Name</th>
-                                <th>Academic Year</th>
-                                <th>Starts</th>
-                                <th>Ends</th>
-                                <th>System</th>
-                                <th>Sequence</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                `;
+		frappe.call({
+			method: "frappe.client.get_list",
+			args: {
+				doctype: "Term Configuration",
+				fields: [
+					"name",
+					"term_name",
+					"academic_year",
+					"starts",
+					"ends",
+					"system",
+					"sequence",
+				],
+				limit_page_length: 100,
+				order_by: "starts desc",
+			},
+			callback(r) {
+				const terms = r.message || [];
 
-                if (terms.length === 0) {
-                    html += '<tr><td colspan="6" class="text-center text-muted">No Terms Found</td></tr>';
-                } else {
-                    terms.forEach(term => {
-                        html += `
-                            <tr style="cursor: pointer;" class="term-row" data-name="${term.name}">
-                                <td>${term.term_name || term.name}</td>
-                                <td>${term.academic_year || '-'}</td>
-                                <td>${frappe.datetime.str_to_user(term.starts) || '-'}</td>
-                                <td>${frappe.datetime.str_to_user(term.ends) || '-'}</td>
-                                <td>${term.system || '-'}</td>
-                                <td>${term.sequence || '-'}</td>
-                            </tr>
-                        `;
-                    });
-                }
+				let html = `
+					<div class="row" style="margin-bottom:15px;">
+						<div class="col-xs-12 text-right">
+							<button class="btn btn-primary btn-add-term">
+								${frappe.utils.icon("add", "sm")} Add Term
+							</button>
+						</div>
+					</div>
 
-                html += '</tbody></table>';
-                $wrapper.html(html);
+					<table class="table table-bordered">
+						<thead style="background:#f5f7fa;">
+							<tr>
+								<th>Term Name</th>
+								<th>Academic Year</th>
+								<th>Starts</th>
+								<th>Ends</th>
+								<th>System</th>
+								<th>Sequence</th>
+							</tr>
+						</thead>
+						<tbody>
+				`;
 
-                // Bind events
-                $wrapper.find('.btn-add-term').on('click', function () {
-                    frappe.set_route('Form', 'Term Configuration', 'new-term-configuration');
-                });
+				if (!terms.length) {
+					html += `
+						<tr>
+							<td colspan="6" class="text-center text-muted">
+								No Terms Found
+							</td>
+						</tr>
+					`;
+				} else {
+					terms.forEach((t) => {
+						html += `
+							<tr class="term-row" data-name="${t.name}" style="cursor:pointer;">
+								<td>${t.term_name || t.name}</td>
+								<td>${t.academic_year || "-"}</td>
+								<td>${t.starts ? frappe.datetime.str_to_user(t.starts) : "-"}</td>
+								<td>${t.ends ? frappe.datetime.str_to_user(t.ends) : "-"}</td>
+								<td>${t.system || "-"}</td>
+								<td>${t.sequence || "-"}</td>
+							</tr>
+						`;
+					});
+				}
 
-                $wrapper.find('.term-row').on('click', function () {
-                    const name = $(this).data('name');
-                    frappe.set_route('Form', 'Term Configuration', name);
-                });
-            }
-        });
-    },
+				html += `</tbody></table>`;
+				$wrapper.html(html);
 
-    render_class_ui: function (frm) {
-        const $wrapper = frm.get_field('class_ui_container').$wrapper;
-        $wrapper.html('<p>Loading Classes...</p>');
+				$wrapper.find(".btn-add-term").on("click", () => {
+					frappe.set_route("Form", "Term Configuration", "new-term-configuration");
+				});
 
-        frappe.call({
-            method: 'frappe.client.get_list',
-            args: {
-                doctype: 'Class Configuration',
-                fields: ['name', 'class_name', 'term', 'programme', 'course', 'type', 'faculty'],
-                limit_page_length: 100,
-                order_by: 'creation desc'
-            },
-            callback: function (r) {
-                const classes = r.message || [];
-                let html = `
-                    <div class="row" style="margin-bottom: 15px;">
-                        <div class="col-xs-12 text-right">
-                            <button class="btn btn-primary btn-add-class">
-                                ${frappe.utils.icon('add', 'sm')} Add Class
-                            </button>
-                        </div>
-                    </div>
-                    <table class="table table-bordered">
-                        <thead style="background-color: #f5f7fa;">
-                            <tr>
-                                <th>Class Name</th>
-                                <th>Term</th>
-                                <th>Programme</th>
-                                <th>Course</th>
-                                <th>Type</th>
-                                <th>Faculty</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                `;
+				$wrapper.find(".term-row").on("click", function () {
+					frappe.set_route(
+						"Form",
+						"Term Configuration",
+						$(this).data("name")
+					);
+				});
+			},
+		});
+	},
 
-<<<<<<< HEAD
-                if (classes.length === 0) {
-                    html += '<tr><td colspan="6" class="text-center text-muted">No Classes Found</td></tr>';
-                } else {
-                    classes.forEach(cls => {
-                        html += `
-                            <tr style="cursor: pointer;" class="class-row" data-name="${cls.name}">
-                                <td>${cls.class_name || cls.name}</td>
-                                <td>${cls.term || '-'}</td>
-                                <td>${cls.programme || '-'}</td>
-                                <td>${cls.course || '-'}</td>
-                                <td>${cls.type || '-'}</td>
-                                <td>${cls.faculty || '-'}</td>
-                            </tr>
-                        `;
-                    });
-                }
+	/* --------------------------------------------------------------------------
+	 * CLASS UI
+	 * -------------------------------------------------------------------------- */
+	render_class_ui(frm) {
+		const $wrapper = frm.get_field("class_ui_container").$wrapper;
+		$wrapper.html("<p>Loading Classes...</p>");
 
-                html += '</tbody></table>';
-                $wrapper.html(html);
+		frappe.call({
+			method: "frappe.client.get_list",
+			args: {
+				doctype: "Class Configuration",
+				fields: [
+					"name",
+					"class_name",
+					"term",
+					"programme",
+					"course",
+					"type",
+					"faculty",
+				],
+				limit_page_length: 100,
+				order_by: "creation desc",
+			},
+			callback(r) {
+				const classes = r.message || [];
 
-                // Bind events
-                $wrapper.find('.btn-add-class').on('click', function () {
-                    frappe.set_route('Form', 'Class Configuration', 'new-class-configuration');
-                });
+				let html = `
+					<div class="row" style="margin-bottom:15px;">
+						<div class="col-xs-12 text-right">
+							<button class="btn btn-primary btn-add-class">
+								${frappe.utils.icon("add", "sm")} Add Class
+							</button>
+						</div>
+					</div>
 
-                $wrapper.find('.class-row').on('click', function () {
-                    const name = $(this).data('name');
-                    frappe.set_route('Form', 'Class Configuration', name);
-                });
-            }
-        });
-    }
-=======
-				if (classes.length === 0) {
-					html += `<tr><td colspan="7" class="text-center text-muted">No Classes Found</td></tr>`;
+					<table class="table table-bordered">
+						<thead style="background:#f5f7fa;">
+							<tr>
+								<th>Class Name</th>
+								<th>Term</th>
+								<th>Programme</th>
+								<th>Course</th>
+								<th>Type</th>
+								<th>Faculty</th>
+							</tr>
+						</thead>
+						<tbody>
+				`;
+
+				if (!classes.length) {
+					html += `
+						<tr>
+							<td colspan="6" class="text-center text-muted">
+								No Classes Found
+							</td>
+						</tr>
+					`;
 				} else {
 					classes.forEach((c) => {
 						html += `
-                            <tr>
-                                <td><a href="/app/student-group/${c.name
-							}" style="font-weight: bold;">${c.student_group_name}</a></td>
-                                <td>${c.class_type || "-"}</td>
-                                <td>${c.course || "-"}</td>
-                                <td>${c.academic_term || "-"}</td>
-                                <td>${c.faculty || "-"}</td>
-                                <td>${c.max_strength || 0}</td>
-                                <td>${c.section || "-"}</td>
-                            </tr>
-                        `;
+							<tr class="class-row" data-name="${c.name}" style="cursor:pointer;">
+								<td>${c.class_name || c.name}</td>
+								<td>${c.term || "-"}</td>
+								<td>${c.programme || "-"}</td>
+								<td>${c.course || "-"}</td>
+								<td>${c.type || "-"}</td>
+								<td>${c.faculty || "-"}</td>
+							</tr>
+						`;
 					});
 				}
+
 				html += `</tbody></table>`;
-				$container.html(html);
-			},
-		});
-	},
+				$wrapper.html(html);
 
-	show_add_single_class_dialog: function (frm) {
-		const d = new frappe.ui.Dialog({
-			title: "Add Single Class",
-			fields: [
-				{
-					label: "Department",
-					fieldname: "department",
-					fieldtype: "Link",
-					options: "Department",
-					reqd: 1,
-				},
-				{
-					label: "Program",
-					fieldname: "program",
-					fieldtype: "Link",
-					options: "Program",
-					reqd: 1,
-				},
-				{
-					label: "Academic Year",
-					fieldname: "academic_year",
-					fieldtype: "Link",
-					options: "Academic Year",
-					reqd: 1,
-				},
-				{
-					label: "Term",
-					fieldname: "academic_term",
-					fieldtype: "Link",
-					options: "Academic Term",
-					reqd: 1,
-				},
-				{ fieldtype: "Column Break" },
-				{
-					label: "Course",
-					fieldname: "course",
-					fieldtype: "Link",
-					options: "Course",
-					reqd: 1,
-				},
-				{
-					label: "Class Type",
-					fieldname: "class_type",
-					fieldtype: "Select",
-					options: "Theory\nPractical\nTutorial\nLab",
-					reqd: 1,
-				},
-				{ label: "Faculty", fieldname: "faculty", fieldtype: "Link", options: "Faculty" },
-				{
-					label: "Capacity",
-					fieldname: "max_strength",
-					fieldtype: "Int",
-					default: 60,
-					reqd: 1,
-				},
-				{
-					label: "Section",
-					fieldname: "section",
-					fieldtype: "Link",
-					options: "Program Batch Section",
-					description: "Optional: Link to a specific batch section",
-				},
-				{
-					label: "Name (Auto-generated if empty)",
-					fieldname: "student_group_name",
-					fieldtype: "Data",
-				},
-			],
-			primary_action_label: "Create",
-			primary_action: function (values) {
-				if (!values.student_group_name) {
-					values.student_group_name = `${values.course}-${values.class_type
-						}-${frappe.datetime.now_date()}`; // Fallback, better to let server handle
-				}
+				$wrapper.find(".btn-add-class").on("click", () => {
+					frappe.set_route("Form", "Class Configuration", "new-class-configuration");
+				});
 
-				frappe.call({
-					method: "slcm.slcm.doctype.academic_management.academic_management.create_class",
-					args: { data: values },
-					callback: function (r) {
-						if (!r.exc) {
-							frappe.msgprint("Class Created Successfully");
-							d.hide();
-							// Refresh list
-							frm.events.load_classes(
-								frm,
-								frm.get_field("class_ui_container").$wrapper
-							);
-						}
-					},
+				$wrapper.find(".class-row").on("click", function () {
+					frappe.set_route(
+						"Form",
+						"Class Configuration",
+						$(this).data("name")
+					);
 				});
 			},
 		});
-		d.show();
 	},
 
-	show_add_bulk_class_dialog: function (frm) {
-		const d = new frappe.ui.Dialog({
-			title: "Add Class by Section (Bulk)",
-			fields: [
-				{
-					label: "Department",
-					fieldname: "department",
-					fieldtype: "Link",
-					options: "Department",
-					reqd: 1,
-				},
-				{
-					label: "Program",
-					fieldname: "program",
-					fieldtype: "Link",
-					options: "Program",
-					reqd: 1,
-				},
-				{
-					label: "Academic Year",
-					fieldname: "academic_year",
-					fieldtype: "Link",
-					options: "Academic Year",
-					reqd: 1,
-				},
-				{
-					label: "Batch",
-					fieldname: "batch",
-					fieldtype: "Link",
-					options: "Student Batch Name",
-					reqd: 1,
-				},
-				{ fieldtype: "Section Break", label: "Configuration" },
-				{
-					label: "Term",
-					fieldname: "academic_term",
-					fieldtype: "Link",
-					options: "Academic Term",
-					reqd: 1,
-				},
-				{
-					label: "Course",
-					fieldname: "course",
-					fieldtype: "Link",
-					options: "Course",
-					reqd: 1,
-				},
-				{
-					label: "Class Type",
-					fieldname: "class_type",
-					fieldtype: "Select",
-					options: "Theory\nPractical\nTutorial\nLab",
-					reqd: 1,
-				},
-				{
-					label: "Faculty",
-					fieldname: "faculty",
-					fieldtype: "Link",
-					options: "Faculty",
-					description: "Default faculty for all sections (can change later)",
-				},
-			],
-			primary_action_label: "Bulk Create",
-			primary_action: function (values) {
-				frappe.call({
-					method: "slcm.slcm.doctype.academic_management.academic_management.create_classes_by_section",
-					args: values,
-					callback: function (r) {
-						if (!r.exc) {
-							frappe.msgprint(r.message);
-							d.hide();
-							// Refresh list
-							frm.events.load_classes(
-								frm,
-								frm.get_field("class_ui_container").$wrapper
-							);
-						}
-					},
-				});
-			},
-		});
-		d.show();
-	},
-
-	render_schedule_ui: function (frm) {
+	/* --------------------------------------------------------------------------
+	 * SCHEDULE UI
+	 * -------------------------------------------------------------------------- */
+	render_schedule_ui(frm) {
 		const $wrapper = frm.get_field("schedule_ui_container").$wrapper;
 
-		// Simple placeholder for Timetable - better implemented with full calendar library later
-		let html = `
+		const html = `
 			<div class="text-center">
-				<p>To view the Class Schedule, please check the <a href="/app/course-schedule">Course Schedule</a> list or Calendar view.</p>
-				<button class="btn btn-default btn-view-calendar">View Calendar</button>
+				<p>
+					To view the Class Schedule, please check the
+					<a href="/app/course-schedule">Course Schedule</a>
+					list or Calendar view.
+				</p>
+				<button class="btn btn-default btn-view-calendar">
+					View Calendar
+				</button>
 			</div>
 		`;
+
 		$wrapper.html(html);
 
-		$wrapper.find(".btn-view-calendar").on("click", function () {
+		$wrapper.find(".btn-view-calendar").on("click", () => {
 			frappe.set_route("List", "Course Schedule", "Calendar");
 		});
 	},
->>>>>>> a41b150a68506b5abfbd7c3aa213eb09f140fe4d
 });
--- slcm/slcm/doctype/academic_management/academic_management.py
+++ slcm/slcm/doctype/academic_management/academic_management.py
@@ -1,9 +1,103 @@
-# Copyright (c) 2026, CU and contributors
-# For license information, please see license.txt
-
 import frappe
 from frappe.model.document import Document
 
-
 class AcademicManagement(Document):
-    pass
+	pass
+
+@frappe.whitelist()
+def create_class(data):
+	if isinstance(data, str):
+		data = frappe.parse_json(data)
+
+	# Check duplicate
+	# Adapting to Class Configuration fields based on JS input
+	# JS input: department, program, academic_year, academic_term, course, class_type, faculty, max_strength, section, student_group_name
+	
+	exists = frappe.db.exists(
+		"Class Configuration",
+		{
+			"class_name": data.get("student_group_name"),
+		},
+	)
+
+	if exists:
+		frappe.throw("A class with this name already exists.")
+
+	doc = frappe.new_doc("Class Configuration")
+	doc.class_name = data.get("student_group_name")
+	doc.department = data.get("department")
+	doc.programme = data.get("program")
+	doc.academic_year = data.get("academic_year")
+	doc.term = data.get("academic_term")
+	doc.course = data.get("course")
+	doc.type = data.get("class_type")
+	doc.faculty = data.get("faculty")
+	doc.capacity = data.get("max_strength")
+	# doc.section = data.get("section") # Assuming section is handled or linked
+	
+	doc.insert()
+	return doc.name
+
+@frappe.whitelist()
+def create_classes_by_section(
+	department, program, academic_year, batch, academic_term, course, class_type, faculty
+):
+	# Enqueue this to run in background
+	frappe.enqueue(
+		"slcm.slcm.doctype.academic_management.academic_management.process_bulk_class_creation",
+		queue="long",
+		timeout=1500,
+		department=department,
+		program=program,
+		academic_year=academic_year,
+		batch=batch,
+		academic_term=academic_term,
+		course=course,
+		class_type=class_type,
+		faculty=faculty,
+		user=frappe.session.user,
+	)
+	return "Bulk creation started. You will be notified upon completion."
+
+def process_bulk_class_creation(
+	department, program, academic_year, batch, academic_term, course, class_type, faculty, user
+):
+	sections = frappe.get_all(
+		"Program Batch Section",
+		filters={
+			"department": department,
+			"program": program,
+			"academic_year": academic_year,
+			"batch": batch,
+		},
+		fields=["name", "section_name", "capacity"],
+	)
+
+	created_count = 0
+	skipped_count = 0
+
+	for section in sections:
+		class_name = f"{course}-{section.section_name}-{class_type}"
+		
+		if frappe.db.exists("Class Configuration", {"class_name": class_name}):
+			skipped_count += 1
+			continue
+
+		doc = frappe.new_doc("Class Configuration")
+		doc.class_name = class_name
+		doc.department = department
+		doc.programme = program
+		doc.academic_year = academic_year
+		doc.term = academic_term
+		doc.course = course
+		doc.type = class_type
+		doc.faculty = faculty
+		doc.capacity = section.capacity
+		# Store section info if field exists, otherwise rely on name
+		
+		doc.insert()
+		created_count += 1
+
+	frappe.publish_realtime(
+		"bulk_class_creation_done", {"created": created_count, "skipped": skipped_count}, user=user
+	)
